410a9c9b93ce8bc0e4c42337cb13b36f28345fbf
Dejan Lavbič <dejan@lavbic.net>
2022-01-31 06:36:14 +0100

    Start of XML -> JSON conversion and translation

--- MOR-app/src/app/components/preview-xml/preview-xml.component.ts
+++ MOR-app/src/app/components/preview-xml/preview-xml.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { XMLParser } from 'fast-xml-parser';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-preview-xml',
   templateUrl: './preview-xml.component.html',
-  styleUrls: ['./preview-xml.component.css']
+  styleUrls: ['./preview-xml.component.css'],
 })
 export class PreviewXmlComponent implements OnInit {
+  faSearch = faSearch;
 
-  constructor() { }
+  @Input('lang') lang!: string;
 
-  ngOnInit(): void {
+  public parser = new XMLParser();
+
+  public xmlInput?: string;
+  public jsOutput?: any;
+  public jsOutputString?: string;
+
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'sl', 'es']);
+    translate.setDefaultLang('en');
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['lang']) this.translate.use(this.lang);
+  }
+
+  ngOnInit(): void {
+    this.xmlInput =
+      '<BirthEvidence><Child><GivenName>Lisa</GivenName><FamilyName>Simpson</FamilyName><Gender>Female</Gender><BirthDate>20/02/2002</BirthDate><PlaceOfBirth><geographicIdentifier></geographicIdentifier><geographicName>Springfield, Earth</geographicName></PlaceOfBirth></Child><Parent1><GivenName>Homer</GivenName><FamilyName>Simpson</FamilyName></Parent1><Parent2><GivenName>Marge</GivenName><FamilyName>Simpson</FamilyName></Parent2></BirthEvidence>';
+
+    this.jsOutput = this.parser.parse(this.xmlInput);
+    this.jsOutputString = JSON.stringify(this.jsOutput, null, 2);
+  }
 }
