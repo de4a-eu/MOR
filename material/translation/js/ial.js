2198348a0729f51bb4f58760cf9937c32b581d74
anarosa.guzman <anarosa.guzman@XP55SGAD284.DSIC.ES>
2022-09-16 13:56:25 +0200

    Improvements and ialAPIrequestList fixed

--- material/translation/js/ial.js
+++ material/translation/js/ial.js
@@ -21,21 +21,19 @@ let ialProvisions = null
  * @value {array} canEvidTokenOptions : a mandatory array with the options of a multiple SELECT element, whose values are canonical evidence type token (e.g. BirthEvidence) and the selected ones are included in the request
  * @value {string} atuCode : an optional string with the code of an administrative territorial unit to request, usually a NUTS-0 code corresponding to the issuing country
  * @value {boolean} asXml :  an optional boolean. "True" forces that the "ialProvisions" text is in XML format; otherwise, it is in JSON format   
- * @return {string} : HTTP status code of the return; "200" is representing a suscessful call
+ * @return {int} : HTTP status code of the return; "200" is representing a suscessful call. "404" when the search has not found results.
  */
 function ialAPIrequestOptions(canEvidTokenOptions,atuCode,asXml) {
-	var urlApi = serverIALapi
+	var tokenList = ""
 	var cont = 0
 	for (var i=0; i<canEvidTokenOptions.length;i++) {
 		if (canEvidTokenOptions[i].selected) {
-			urlApi += (cont>0?",":"")+canEvidUrn+canEvidTokenOptions[i].value
+			tokenList += (cont>0?",":"")+canEvidTokenOptions[i].value
 			cont++
 		}
 	}
-	if (atuCode != "") {
-		urlApi += "/" + atuCode
-	}
-	return retrieveIALProvisions (urlApi, (asXml==true?true:false))
+	if (debug) console.log("[DEBUG]<ialAPIrequestOptions> " + tokenList + "("+cont+"/"+canEvidTokenOptions.length+")")
+	return ialAPIrequestList (tokenList, atuCode, asXml)
 }
 
 /**
@@ -44,10 +42,21 @@ function ialAPIrequestOptions(canEvidTokenOptions,atuCode,asXml) {
  * @value {list} canEvidTokenList : a mandatory comma-separated list with the canonical evidence type tokens to request (e.g. BirthEvidence)
  * @value {string} atuCode : an optional string with the code of an administrative territorial unit to request, usually a NUTS-0 code corresponding to the issuing country
  * @value {boolean} asXml :  an optional boolean. "True" forces that the "ialProvisions" text is in XML format; otherwise, it is in JSON format   
- * @return {string} : HTTP status code of the return; "200" is representing a suscessful call
+ * @return {int} : HTTP status code of the return; "200" is representing a suscessful call. "404" when the search has not found results.
  */
 function ialAPIrequestList(canEvidTokenList,atuCode,asXml) {
-	var urlApi = serverIALapi + canEvidUrn + canEvidTokenList
+	var urlApi = serverIALapi 
+	if (canEvidTokenList.indexOf(",")>0) {
+		var canEvidTokenArray = canEvidTokenList.split(",")
+		if (debug) console.log("[DEBUG]<ialAPIrequestList> " + canEvidTokenArray.length)
+		var cont=0
+		for (var i=0; i<canEvidTokenArray.length;i++) {
+			urlApi += (cont>0?",":"")+canEvidUrn+canEvidTokenArray[i]
+			cont++
+		}
+	} else {
+		urlApi += canEvidUrn+canEvidTokenList
+	}
 	if (atuCode != "") {
 		urlApi += "/" + atuCode
 	}
@@ -59,7 +68,7 @@ function ialAPIrequestList(canEvidTokenList,atuCode,asXml) {
  * The response text is stored in the variable "ialProvisions" or null if an error is returned
  * @value {string} urlApi : a mandatory string with the final URL of the request
  * @value {boolean} asXml :  an optional boolean. "True" forces that the "ialProvisions" text is in XML format; otherwise, it is in JSON format   
- * @return {string} : HTTP status code of the return; "200" is representing a suscessful call
+ * @return {int} : HTTP status code of the return; "200" is representing a suscessful call. "404" when the search has not found results.
  */
 function retrieveIALProvisions (urlApi, asXml) {
 	const xmlhttp = new XMLHttpRequest()
@@ -73,6 +82,7 @@ function retrieveIALProvisions (urlApi, asXml) {
 				if (debug) console.log("[DEBUG]<retrieveIALProvisions> Response: ", this.responseType +"\n "+this.response)
 				if (this.response.indexOf("ERROR")>0) {
 					console.log("[ERROR]<retrieveIALProvisions> ERROR with HTTP/200: ", this.status +"\n"+this.response)
+					return (this.responseText.indexOf("no-match")>0?404:418)
 				} else
 					ialProvisions = this.responseText 
 			}
